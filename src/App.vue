9e9f8195e5db73a1f41e21c5ccbde755e3a96f2a
Tornike Jandieri <108745129+tornikejandieri@users.noreply.github.com>
2022-09-25 18:01:23 +0400

    some more Vue reference

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <TheHeader title="RememberMe" />
+  <TheResource />
+</template>
+
+<script>
+import TheResource from './components/learning-resources/TheResource.vue';
+import TheHeader from './components/layouts/TheHeader.vue';
+export default {
+  components: { TheHeader, TheResource },
+};
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
+
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: 'Roboto', sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
